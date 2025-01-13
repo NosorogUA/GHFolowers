131a4f16ef43fd2789e1ecd9be8bee204259e536
NosorogUA <tokalenko.flp@gmail.com>
2025-01-13 10:09:32 +0200

    Add date formater

--- GHFolowers/Screens/UserInfo/UserInfoVC.swift
+++ GHFolowers/Screens/UserInfo/UserInfoVC.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 16
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private lazy var dateLabel = GFBodyLabel(textAlignment: .center)
+    
     var userName: String!
 
     override func viewDidLoad() {
@@ -16,7 +27,6 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dissmissVC))
         navigationItem.rightBarButtonItem = doneButton
-//        title = userName
         getUser()
         // Do any additional setup after loading the view.
     }
@@ -37,23 +47,46 @@ class UserInfoVC: UIViewController {
     
     private func showInfo(_ user: User) {
         DispatchQueue.main.async {
-            let stackView = UIStackView(arrangedSubviews: [
-                UserInfoHeaderView(user: user),
-                GFInfoView(type: .repo(countRepo: user.publicRepos, countGists: user.publicGists)),
-                GFInfoView(type: .followers(followers: user.followers, following: user.following)),
-                UIView()])
+            let items = [UserInfoHeaderView(user: user),
+                         GFInfoView(type: .repo(countRepo: user.publicRepos, countGists: user.publicGists)),
+                         GFInfoView(type: .followers(followers: user.followers, following: user.following)),
+                         self.dateLabel,
+                         UIView()]
             
-            stackView.distribution = .fill
-            stackView.alignment = .fill
-            stackView.spacing = 16
-            stackView.axis = .vertical
+            if let date = Date.fromISOStringToDate(user.createdAt) {
+                let formattedDate = date.toCustomFormattedString()
+                self.dateLabel.text = "GitHub since: " + formattedDate
+            }
             
-            self.view.addSubview(stackView)
-            stackView.constrainToBounds(top: 50, left: 20, bottom: 20, right: 20)
+            items.forEach {
+                self.stackView.addArrangedSubview($0)
+            }
+            
+            self.view.addSubview(self.stackView)
+            self.stackView.constrainToBounds(top: 50, left: 20, bottom: 20, right: 20)
         }
     }
 
     @objc func dissmissVC() {
         dismiss(animated: true)
+    }
+}
+
+
+extension Date {
+    
+    // Function to convert a date string to a specific format
+    static func fromISOStringToDate(_ isoString: String) -> Date? {
+        let isoFormatter = DateFormatter()
+        isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ" // Input format of the ISO string
+        return isoFormatter.date(from: isoString)
+    }
+    
+    // Function to convert the date into "DD.month.YYYY" format
+    func toCustomFormattedString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy" // Output format (e.g., "26.April.2009")
+        dateFormatter.locale = Locale(identifier: "en_US") // Ensure English month names
+        return dateFormatter.string(from: self)
     }
 }
